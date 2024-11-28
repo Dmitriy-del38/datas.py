98219ac51996a989f558922ace9d5b94c5938f2f
Dmitriy-del38 <mashin.dmitriy1985@gmail.com>
2024-11-28 08:58:36 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,12 @@
+name='Dmitriy'
+age=38
+a=12
+print(a)
+b=1.5
+print(b)
+c='python'
+print(c)
+d=b/a
+print(d)
+print(c,',''всего задач :,',a,'затрачено часов :,',b,',''среднее время выполнения,',d,'.')
+
