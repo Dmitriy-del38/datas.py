a57d1622fd787abc91286418332208a0b9f9a8bb
Dmitriy-del38 <mashin.dmitriy1985@gmail.com>
2024-12-03 08:52:22 +0300

    Add files via upload

---
+++ 'module_1_5'.py
@@ -0,0 +1,10 @@
+#immutable_var =("молоток","пила","лом",6,7,8)
+#print(immutable_var)
+#print(type(immutable_var))
+#print(immutable_var[2])
+#immutable_var[2] = 4
+
+mutable_list=["молоток","пила","лом"]
+print(mutable_list)
+mutable_list[0]="топор"
+print(mutable_list)
